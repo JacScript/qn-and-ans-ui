dd26a542618c335d5bed893564e0fdd2dccffbf0
JacScript <jacksonsylas59@gmail.com>
2024-07-27 22:26:03 +0300

    minor changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -17,13 +17,13 @@ const Header = () => {
   const [user, setUser] = useState(getUser());
 
   return (
-    <div className="text-white flex  shadow-sm shadow-[rgba(0,0,0,.2)]">
-      <div className="flex-auto w-2 bg-slate-600">
+    <div className="text-white flex w-full shadow-sm shadow-[rgba(0,0,0,.2)]">
+      <div className="flex-[1]">
         <Link
           to={"/"}
-          className="h-[60px] leading-[30px] inline-block text-[#fff] py-0 px-[15px]"
+          className="h-full pb-[8px]  flex justify-center items-center text-[#fff] py-0 px-[px]"
         >
-          <i className="fa-brands fa-stack-overflow text-[30px] mt-[10px] inline-block float-left" />
+          <i className="fa-brands fa-stack-overflow mt-[10px] text-[30px] inline-block float-left" />
           <a href="#" className="pl-[5px] inline-block pt-[15px] font-[300]">
             {" "}
             stack <span className="font-normal">overcloned</span>
@@ -31,7 +31,7 @@ const Header = () => {
         </Link>
       </div>
 
-      <div className="bg-white flex-auto w-64">
+      <div className=" flex-[4] w-64 pb-[12px] px-2">
         <form action="" className="">
           <input
             className="w-full rounded-[3px] border border-gray-700 bg-[rgba(0,0,0,.1)] py-[10px] px-[6px] mt-[9px]"
@@ -41,14 +41,14 @@ const Header = () => {
         </form>
       </div>
 
-      <div className=" flex-auto w-14">
+      <div className=" flex-[1] h-full">
         {user ? (
-          <div className="flex justify-start items-center text-white">
+          <div className="text-center pb-[px] w-full h-full mt-[25px]">
             {user.username}
           </div>
         ) : (
-          <div className="flex justify-start items-center">
-            <Link to="/login" className="text-center">
+          <div className="text-center pb-[px] w-full h-full mt-[25px]">
+            <Link to="/login" className="">
               Log In
             </Link>
           </div>
